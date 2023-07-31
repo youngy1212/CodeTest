6a534a9c4e0e0cd806c92a0106e9eef8243f7ec1
youngy1212 <117425085+youngy1212@users.noreply.github.com>
2023-07-31 15:58:32 +0900

    7.31

--- Level0.java
+++ Level0.java
@@ -59,8 +59,85 @@ public class Level0 {
         }else{
             System.out.print(n+" is odd");
         }
+   
+
+    /* 문자열 섞기 */
+    for(int i = 0; i < str1.length(); i++){
+        answer+= str1.charAt(i);
+        answer+= str2.charAt(i);
     }
 
+    /*문자 리스트를 문자열로 변환하기 */
+    for(int i=0; i < arr.length; i++ ){
+        answer += arr[i];
 
+        // return String.join("", arr); 도 사용가능 join함수
+    }
+
+    //문자열 곱하기 -> 문자열을 k만큼 반복
+    //return my_string.repeat(k);
+
+    }
+
+    
+    public int solution(int a, int b) {
+
+        /*더 큰값을 합치기 */
+        String strA = String.valueOf(a);
+        String strB = String.valueOf(b);
+        String strSum1 = strA + strB;
+        String strSum2 = strB + strA;
+       
+        Math.max(Integer.valueOf(strSum1), Integer.valueOf(strSum2));
+
+        /*두 수의 연산값 비교하기  */
+        /* max함수의 경우 두값이 같을 경우 앞의 값을 return함 */
+        Math.max(Integer.parseInt(String.valueOf(a)+String.valueOf(b)),2*a*b);
+
+       /*n의 배수일 경우*/
+       int answer = 0;
+        
+       if(num%n == 0){
+           answer = 1;
+       }
+
+       /* 공배수일 경우 */
+       return number % n == 0 && number % m == 0 ? 1 : 0;
+
+      
+
+    }
+
+     
+     class Solution {
+        public int solution(String ineq, String eq, int n, int m) {
+
+            /*홀짝에 따라 다른 값 반환 */
+            for(int i = n; i >= 0; i -= 2)
+            answer += (n % 2 == 0) ? i * i : i;
+            
+            /* 조건 문자열*/
+            // if (ineq.equals("<") && eq.equals("=")) {
+            // return n <= m ? 1 : 0;
+            // }
+        
+            // if (ineq.equals("<")) {
+            //     return n < m ? 1 : 0;
+            // }
+        
+            // if (ineq.equals(">") && eq.equals("=")) {
+            //     return n >= m ? 1 : 0;
+            // }
+        
+            // if (ineq.equals(">")) {
+            //     return n > m ? 1 : 0;
+            // }    
+        
+
+             /*flag에 따라 다른 값 반환하기 */
+             return flag ? a + b : a - b;
+        }
+    }
+     
 
 }
